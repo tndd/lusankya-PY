e5a1e8d0153979f4fd936005736dbfff9f6f5f97
tau <36781951+tndd@users.noreply.github.com>
2023-12-18 21:06:10 +0900

    mv

---
+++ infra/api/alpaca/alpaca_cli.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass
+from enum import Enum
+
+from infra.api.client import ApiClient, ApiSnapshot
+
+
+class AlpacaEndpoint(str, Enum):
+    ASSET = 'https://broker-api.sandbox.alpaca.markets/v1/assets'
+    BAR = "https://data.alpaca.markets/v2/stocks/bars"
+
+
+@dataclass
+class AlpacaApiClient(ApiClient):
+    key: str
+    secret: str
+
+    @property
+    def header_alpaca(self):
+        return {
+            "APCA-API-KEY-ID": self.key,
+            "APCA-API-SECRET-KEY": self.secret
+        }
+
+    def get_as_alpaca(self, endpoint: AlpacaEndpoint, query: dict) -> ApiSnapshot:
+        return self.get(endpoint.value, query)
--- infra/api/client.py
+++ infra/api/client.py
@@ -1,12 +1,6 @@
 from dataclasses import dataclass
-from enum import Enum
 
 import requests
-
-
-class AlpacaEndpoint(str, Enum):
-    ASSET = 'https://broker-api.sandbox.alpaca.markets/v1/assets'
-    BAR = "https://data.alpaca.markets/v2/stocks/bars"
 
 
 @dataclass
@@ -30,19 +24,3 @@ class ApiClient:
             dict(r.headers),
             r.text
         )
-
-
-@dataclass
-class AlpacaApiClient(ApiClient):
-    key: str
-    secret: str
-
-    @property
-    def header_alpaca(self):
-        return {
-            "APCA-API-KEY-ID": self.key,
-            "APCA-API-SECRET-KEY": self.secret
-        }
-
-    def get_as_alpaca(self, endpoint: AlpacaEndpoint, query: dict) -> ApiSnapshot:
-        return self.get(endpoint.value, query)
