a98888c2436ab405e70aacce30266bd74ae82f5f
tau <36781951+tndd@users.noreply.github.com>
2023-12-18 16:48:37 +0900

    add test psql client

---
+++ tests/infra/db/psql/client_test.py
@@ -0,0 +1,24 @@
+import os
+
+import pytest
+from dotenv import load_dotenv
+
+from infra.db.psql.client import PsqlClient
+
+load_dotenv()
+
+
+def test_generate_psql_client():
+    assert PsqlClient(url=os.getenv('PSQL_URL_TEST'))
+
+
+def test_transact_execute():
+    psql_cli = PsqlClient(url=os.getenv('PSQL_URL_TEST'))
+    queries = ['SELECT 1' for _ in range(4)]
+    assert psql_cli.transact_execute(queries) == None
+
+
+def test_parallel_execute():
+    psql_cli = PsqlClient(url=os.getenv('PSQL_URL_TEST'))
+    queries = ['SELECT 1' for _ in range(10)]
+    assert psql_cli.parallel_execute(queries) == None
