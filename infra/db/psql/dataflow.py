6186e6a0f68b8794bd8790608443f8a1233a07fd
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2023-12-20 17:22:04 +0900

    add empty dataflow functions

--- infra/db/psql/dataflow.py
+++ infra/db/psql/dataflow.py
@@ -24,3 +24,26 @@ class ApiQuerySchedule:
     api_schedule_id: int
     timestamp: str
     query: str
+
+
+def regist_api_schedule(endpoint: str, params: dict, header: dict):
+    """
+    Record and schedule all the necessary information for api execution.
+    """
+    pass
+
+
+def execute_scheduled_api(api_schedule_id: str):
+    """
+    Executes the api of the specified id.
+    If it is one that has not been executed or has failed.
+    """
+    pass
+
+
+def execute_scheduled_queries():
+    """
+    Execute all scheduled queries.
+    For queries that have been successfully executed, they are deleted.
+    """
+    pass
