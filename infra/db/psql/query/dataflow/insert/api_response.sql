c570b98e811b4459177e1729c69138f335a436bd
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2023-12-24 21:32:08 +0900

    add sql insert dataflow

---
+++ infra/db/psql/query/dataflow/insert/api_response.sql
@@ -0,0 +1,3 @@
+INSERT INTO dataflow.api_response
+(api_schedule_id, status, resp_header, body)
+VALUES(%s, %s, %s, %s);
---
+++ infra/db/psql/query/dataflow/insert/api_schedule.sql
@@ -0,0 +1,3 @@
+INSERT INTO dataflow.api_schedule
+(endpoint, params, req_header)
+VALUES(%s, %s, %s);
