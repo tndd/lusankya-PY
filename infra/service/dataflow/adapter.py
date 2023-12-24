0acac929120fbd7a084b63049a0e8b00f4568ffc
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2023-12-24 19:21:57 +0900

    add adapter snapshot

--- infra/service/dataflow/adapter.py
+++ infra/service/dataflow/adapter.py
@@ -1,10 +1,37 @@
 from infra.api.client import ApiSnapshot
 from .model import ApiSchedule, ApiResponse
+from typing import Tuple
 
 
 def snapshot_to_schedule(snapshot: ApiSnapshot) -> ApiSchedule:
-    pass
+    return ApiSchedule(
+        endpoint=snapshot.endpoint,
+        params=snapshot.query,
+        header=snapshot.header
+    )
 
 
-def snapshot_to_response(snapshot: ApiSnapshot) -> ApiResponse:
-    pass
+def snapshot_to_response(snapshot: ApiSnapshot, api_schedule_id: str) -> ApiResponse:
+    return ApiResponse(
+        api_schedule_id=api_schedule_id,
+        status=snapshot.r_status,
+        header=snapshot.r_header,
+        body=snapshot.r_body
+    )
+
+
+def snapshot_to_schedule_and_response(
+        snapshot: ApiSnapshot
+    ) -> Tuple[ApiSchedule, ApiResponse]:
+    api_schedule = ApiSchedule(
+        endpoint=snapshot.endpoint,
+        params=snapshot.query,
+        header=snapshot.header
+    )
+    api_response = ApiResponse(
+        api_schedule_id=api_schedule._id,
+        status=snapshot.r_status,
+        header=snapshot.r_header,
+        body=snapshot.r_body
+    )
+    return api_schedule, api_response
