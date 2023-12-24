9fe47497a8ee3b9cde679bdd22100a11ff78e9ed
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2023-12-24 20:04:52 +0900

    add test snapshot_to_response

--- tests/infra/service/dataflow/adapter_test.py
+++ tests/infra/service/dataflow/adapter_test.py
@@ -20,7 +20,32 @@ def test_snapshot_to_schedule():
         r_header={},
         r_body='body'
     )
-    api_schedule = snapshot_to_schedule(snapshot)
+    api_schedule: ApiSchedule = snapshot_to_schedule(snapshot)
     assert api_schedule.endpoint == ENDPOINT
     assert api_schedule.params == PARAMS
     assert api_schedule.header == HEADER
+
+
+def test_snapshot_to_response():
+    ENDPOINT = 'https://api.alpaca.markets/v2/assets'
+    R_STATUS = 200
+    R_HEADER = {
+        'r_header_01': 'r_header_01_value',
+        'r_header_02': 'r_header_02_value'
+    }
+    BODY = 'BODY'
+    snapshot = ApiSnapshot(
+        endpoint=ENDPOINT,
+        query={},
+        header={},
+        r_status=R_STATUS,
+        r_header=R_HEADER,
+        r_body=BODY
+    )
+    api_schedule_id = '8ad823f6-0a9a-685a-0099-4495034aeb37'
+    api_response: ApiResponse = snapshot_to_response(snapshot, api_schedule_id)
+    assert api_response.api_schedule_id == api_schedule_id
+    assert api_response.status == R_STATUS
+    assert api_response.header == R_HEADER
+    assert api_response.body == BODY
+
