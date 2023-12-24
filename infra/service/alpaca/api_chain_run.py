a979f614819edb27c3791b76fc320420a3fe8bb0
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2023-12-24 18:07:01 +0900

    add tmp api chain run

---
+++ infra/service/alpaca/api_chain_run.py
@@ -0,0 +1,13 @@
+from infra.api.alpaca.bar import AlpacaBarClient, QueryBar
+from infra.api.client import ApiSnapshot
+
+
+def chain_request_bar(cli: AlpacaBarClient, query: QueryBar):
+    while True:
+        snapshot: ApiSnapshot = cli.get_bar(query)
+        # snapshotの結果をScheduleとResponseの両方に保存
+        # TODO: 保存処理
+        if not 'next_page_token' in snapshot.r_body:
+            break # 次のページがない場合は終了
+        # query_barのnext_page_tokenを更新して再実行
+        query.page_token = snapshot.r_body['next_page_token']
