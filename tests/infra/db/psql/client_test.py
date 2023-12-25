4d760b3a91e86fd8f4375607b0c71a7a3a33739b
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2023-12-25 23:51:59 +0900

    update client_test

--- tests/infra/db/psql/client_test.py
+++ tests/infra/db/psql/client_test.py
@@ -3,34 +3,32 @@ import os
 from dotenv import load_dotenv
 
 from infra.db.psql.client import PsqlClient
+from tests.conftest import psql_client
 
 load_dotenv()
 
 
-def test_generate_psql_client():
-    assert PsqlClient(url=os.getenv('PSQL_URL_TEST'))
+def test_generate_psql_client(psql_client):
+    assert psql_client == PsqlClient(url=os.getenv('PSQL_URL_TEST'))
 
 
-def test_execute():
+def test_execute(psql_client):
     """
     以下２点の要素を確認する。
     1. 単発のクエリを実行出来ていること
     2. クエリの結果が取得できていること
     """
-    psql_cli = PsqlClient(url=os.getenv('PSQL_URL_TEST'))
-    assert psql_cli.execute('SELECT 1') == [(1,)]
+    assert psql_client.execute('SELECT 1') == [(1,)]
 
 
-def test_execute_queries():
-    psql_cli = PsqlClient(url=os.getenv('PSQL_URL_TEST'))
+def test_execute_queries(psql_client):
     queries = ['SELECT 1' for _ in range(4)]
-    assert psql_cli.execute_queries(queries) == None
+    assert psql_client.execute_queries(queries) == None
 
 
-def test_parallel_execute():
-    psql_cli = PsqlClient(url=os.getenv('PSQL_URL_TEST'))
+def test_parallel_execute(psql_client):
     queries = ['SELECT 1' for _ in range(10)]
-    assert psql_cli.parallel_execute(queries) == None
+    assert psql_client.parallel_execute(queries) == None
 
 
 def test_parallel_execute_change_worker_num():
@@ -42,20 +40,19 @@ def test_parallel_execute_change_worker_num():
     assert psql_cli.parallel_execute(queries) == None
 
 
-def test_parallel_executemany():
-    psql_cli = PsqlClient(url=os.getenv('PSQL_URL_TEST'))
+def test_parallel_executemany(psql_client):
     table_name = 'usksxcdw'
     try:
         # テスト用の一時的なテーブルを作成
-        psql_cli.execute(f"CREATE TABLE IF NOT EXISTS {table_name} (column1 varchar(255), column2 varchar(255))")
+        psql_client.execute(f"CREATE TABLE IF NOT EXISTS {table_name} (column1 varchar(255), column2 varchar(255))")
         # テスト用投入データを作成
         query = f"INSERT INTO {table_name} (column1, column2) VALUES (%s, %s)"
         data = [(f"value1_{i}", f"value2_{i}") for i in range(10)]
         # executemanyの動作確認
-        assert psql_cli.executemany(query, data) == None
+        assert psql_client.executemany(query, data) == None
         # テーブルにデータが入っていることを確認
-        rows = psql_cli.execute(f'select * from {table_name}')
+        rows = psql_client.execute(f'select * from {table_name}')
         assert len(rows) == 10
     finally:
         # テスト用テーブルは必ず削除しておく
-        psql_cli.execute(f"DROP TABLE IF EXISTS {table_name}")
+        psql_client.execute(f"DROP TABLE IF EXISTS {table_name}")
