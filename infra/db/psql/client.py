894e23b5ba31a467d24c12af0447c3ec85f32871
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2023-12-25 14:51:38 +0900

    upd client psql

--- infra/db/psql/client.py
+++ infra/db/psql/client.py
@@ -1,32 +1,39 @@
 from concurrent.futures import ProcessPoolExecutor
-from dataclasses import dataclass
 from typing import List
 
 from psycopg2 import connect, sql
+from os import getenv
 
 
-@dataclass
-class PsqlClient:
-    url: str
-
-    def transact_execute(self, queries: List[str]):
-        conn = connect(self.url)
+def transact(f):
+    def _wrapper(*args, **kwargs):
+        conn = connect(getenv('PSQL_URL'))
         cur = conn.cursor()
         try:
-            for query in queries:
-                cur.execute(sql.SQL(query))
-            conn.commit()
+            return f(cur, *args, **kwargs)
         except Exception as e:
             conn.rollback()
             raise e
         finally:
             cur.close()
             conn.close()
+    return _wrapper
 
-    def parallel_execute(self, queries: List[str], n_max_worker: int = 8):
-        n_query = len(queries)
-        n_process = min(n_query, n_max_worker)
-        with ProcessPoolExecutor(max_workers=n_process) as executor:
-            for i in range(n_process):
-                chunk = queries[i::n_process]
-                executor.submit(self.transact_execute, chunk)
+
+@transact
+def execute_queries(_cur, queries: List[str]):
+    """
+    _curはtransactデコレータ内で作成されたものが渡されるため、
+    この関数の実行時に_curを渡す必要はなく、queriesのみ渡せば良い。
+    """
+    for query in queries:
+        _cur.execute(sql.SQL(query))
+
+
+def parallel_execute(queries: List[str], n_max_worker: int = 8):
+    n_query = len(queries)
+    n_process = min(n_query, n_max_worker)
+    with ProcessPoolExecutor(max_workers=n_process) as executor:
+        for i in range(n_process):
+            chunk = queries[i::n_process]
+            executor.submit(execute_queries, chunk)
