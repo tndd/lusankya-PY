7e99f594b78f6b462758e8610bb7a15d201b5877
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2023-12-25 15:08:52 +0900

    add method execute many

--- infra/db/psql/client.py
+++ infra/db/psql/client.py
@@ -6,11 +6,22 @@ from os import getenv
 
 
 def transact(f):
+    """
+    概要:
+        データベースへの接続およびトランザクション処理をラップするデコレータ。
+
+    Note:
+        このデコレータを適用した関数には、自動的に第一引数にcursorオブジェクトが渡される。
+        そのためデコレータ適用先の関数呼び出しの際、引数にはcursorオブジェクトを渡す必要はない。
+        適用先関数だけ見れば、どこから_curが発生したのか分からなくなるので注意。
+    """
     def _wrapper(*args, **kwargs):
         conn = connect(getenv('PSQL_URL'))
         cur = conn.cursor()
         try:
-            return f(cur, *args, **kwargs)
+            result = f(cur, *args, **kwargs)
+            conn.commit()
+            return result
         except Exception as e:
             conn.rollback()
             raise e
@@ -22,12 +33,13 @@ def transact(f):
 
 @transact
 def execute_queries(_cur, queries: List[str]):
-    """
-    _curはtransactデコレータ内で作成されたものが渡されるため、
-    この関数の実行時に_curを渡す必要はなく、queriesのみ渡せば良い。
-    """
     for query in queries:
         _cur.execute(sql.SQL(query))
+
+
+@transact
+def execute_many(_cur, query: str, data: List[tuple]):
+    _cur.executemany(query, data)
 
 
 def parallel_execute(queries: List[str], n_max_worker: int = 8):
@@ -37,3 +49,12 @@ def parallel_execute(queries: List[str], n_max_worker: int = 8):
         for i in range(n_process):
             chunk = queries[i::n_process]
             executor.submit(execute_queries, chunk)
+
+
+def parallel_executemany(query: str, data: List(tuple), n_max_worker: int = 8):
+    n_data = len(data)
+    n_process = min(n_data, n_max_worker)
+    with ProcessPoolExecutor(max_workers=n_process) as executor:
+        for i in range(n_process):
+            chunk = data[i::n_process]
+            executor.submit(execute_many, chunk)
