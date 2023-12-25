f89bdc440cc53721b3f575794d143da0d8e093b6
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2023-12-26 00:05:58 +0900

    add new test

--- tests/infra/db/psql/migrate_test.py
+++ tests/infra/db/psql/migrate_test.py
@@ -1,10 +1,36 @@
 from dotenv import load_dotenv
 
 from infra.db.psql.client import PsqlClient
-from infra.db.psql.migrate import migrate
+from infra.db.psql.migrate import (
+    migrate,
+    queries_extension,
+    queries_schema,
+    queries_table,
+    queries_view,
+)
 from tests.conftest import psql_client
 
 load_dotenv()
+
+
+def test_queries_extension():
+    result = queries_extension()
+    assert isinstance(result, list)
+
+
+def test_queries_schema():
+    result = queries_schema()
+    assert isinstance(result, list)
+
+
+def test_queries_table():
+    result = queries_table()
+    assert isinstance(result, list)
+
+
+def test_queries_view():
+    result = queries_view()
+    assert isinstance(result, list)
 
 
 def test_migrate(psql_client: PsqlClient):
