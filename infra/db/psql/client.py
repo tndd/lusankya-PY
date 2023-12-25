794fc66a63c83def89f4860c2f98a4f8beeeb6e1
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2023-12-25 23:13:41 +0900

    add test_parallel_executemany

--- infra/db/psql/client.py
+++ infra/db/psql/client.py
@@ -1,7 +1,7 @@
 from concurrent.futures import ProcessPoolExecutor
-from typing import List, Tuple
+from typing import List, Any
 
-from psycopg2 import connect, sql
+from psycopg2 import connect
 from dataclasses import dataclass
 
 @dataclass
@@ -25,11 +25,16 @@ class PsqlClient:
 
     def calc_optimum_process_num(self, tasks: list) -> int:
         return min(len(tasks), self.n_max_worker)
+    
+    def execute(self, query: str) -> Any:
+        def _f(_cur, query):
+            _cur.execute(query)
+        return self.transact(_f, query)
 
-    def execute(self, queries: List[str]):
+    def execute_queries(self, queries: List[str]):
         def _f(_cur, queries):
             for query in queries:
-                _cur.execute(sql.SQL(query))
+                _cur.execute(query)
         self.transact(_f, queries)
 
     def executemany(self, query: str, data: list):
@@ -42,7 +47,7 @@ class PsqlClient:
         with ProcessPoolExecutor(max_workers=n_process) as executor:
             for i in range(n_process):
                 chunk = queries[i::n_process]
-                executor.submit(self.execute, chunk)
+                executor.submit(self.execute_queries, chunk)
 
     def parallel_executemany(self, query: str, data: list):
         n_process = self.calc_optimum_process_num(data)
--- tests/infra/db/psql/client_test.py
+++ tests/infra/db/psql/client_test.py
@@ -13,8 +13,13 @@ def test_generate_psql_client():
 
 def test_execute():
     psql_cli = PsqlClient(url=os.getenv('PSQL_URL_TEST'))
+    assert psql_cli.execute('SELECT 1') == None
+
+
+def test_execute_queries():
+    psql_cli = PsqlClient(url=os.getenv('PSQL_URL_TEST'))
     queries = ['SELECT 1' for _ in range(4)]
-    assert psql_cli.execute(queries) == None
+    assert psql_cli.execute_queries(queries) == None
 
 
 def test_parallel_execute():
@@ -29,3 +34,18 @@ def test_parallel_execute_change_worker_num():
     )
     queries = ['SELECT 1' for _ in range(32)]
     assert psql_cli.parallel_execute(queries) == None
+
+def test_parallel_executemany():
+    psql_cli = PsqlClient(url=os.getenv('PSQL_URL_TEST'))
+    table_name = 'usksxcdw'
+    try:
+        # テスト用の一時的なテーブルを作成
+        psql_cli.execute(f"CREATE TABLE {table_name} (column1 varchar(255), column2 varchar(255))")
+        # テスト用投入データを作成
+        query = f"INSERT INTO {table_name} (column1, column2) VALUES (%s, %s)"
+        data = [(f"value1_{i}", f"value2_{i}") for i in range(1000)]
+        # executemanyの動作確認
+        assert psql_cli.executemany(query, data) == None
+    finally:
+        # テスト用テーブルは必ず削除しておく
+        psql_cli.execute(f"DROP TABLE IF EXISTS {table_name}")
