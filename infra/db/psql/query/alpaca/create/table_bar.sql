0ef716b22c0013fb9e7d794780728af071f1954c
tau <36781951+tndd@users.noreply.github.com>
2023-12-28 00:15:16 +0900

    update table alpaca.bar

--- infra/db/psql/query/alpaca/create/table_bar.sql
+++ infra/db/psql/query/alpaca/create/table_bar.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS alpaca.bar (
     time_stamp TIMESTAMP WITH TIME ZONE NOT NULL,
     time_frame TEXT NOT NULL,
     symbol TEXT NOT NULL,
+    adjustment TEXT NOT NULL,
     open DOUBLE PRECISION,
     high DOUBLE PRECISION,
     low DOUBLE PRECISION,
@@ -9,7 +10,7 @@ CREATE TABLE IF NOT EXISTS alpaca.bar (
     trade_count BIGINT,
     volume BIGINT,
     vwap DOUBLE PRECISION,
-    PRIMARY KEY(time_stamp, time_frame, symbol)
+    PRIMARY KEY(time_stamp, time_frame, symbol, adjustment)
 );
 
 SELECT create_hypertable('alpaca.bar', 'time_stamp', if_not_exists => TRUE);
