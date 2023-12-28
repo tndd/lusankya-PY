c2d15e3686d6e538bf197f2dbbc2a5f3eeb5490a
tau <36781951+tndd@users.noreply.github.com>
2023-12-28 18:44:39 +0900

    fix comment

--- infra/service/dataflow/api/execute.py
+++ infra/service/dataflow/api/execute.py
@@ -1,13 +1,13 @@
 def execute_api(api_request_id: str):
     """
-    実行予定に登録されたAPIをID指定で実行する
-    未実行または失敗しているものしか実行しないようにする
+    idで指定されたAPIを実行する
+    未実行または失敗しているAPIを実行する
     """
     pass
 
 
 def multi_execute_api():
     """
-    実行予定に登録されているAPIを全て実行する
+    未実行または失敗しているAPIを実行する
     """
     pass
