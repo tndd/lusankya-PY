a6e0069dc22a043845f717504439e44e68603d9b
tau <36781951+tndd@users.noreply.github.com>
2023-12-30 02:01:45 +0900

    tmp

--- domain/dataset/repository/bar.py
+++ domain/dataset/repository/bar.py
@@ -13,8 +13,17 @@ class BarRepository:
 
     def load_from_apiflow(self):
         """
-        ApiFlowの正常完了している結果すべてを自身のテーブルに保存する
+        ApiFlowの正常完了かつ未移動のAPIリクエスト結果すべてをBarテーブルに保存する
+
+        サービスじみた挙動をしているが、
+        単純な動作ではあることとBarテーブルへの情報の保存行為、
+        何よりトランザクション処理の必要性によりリポジトリとして実装する妥協をしている。
         """
+        # TODO
+        # 正常完了かつデータ未移動であるAPIリクエストの結果を全て取得
+        # 各リクエストの結果のbodyをbarテーブル形式に変換し保存
+        #   - リクエストごとに並列処理
+        # 　- bodyの移行と移動元bodyの削除はトランザクション処理する
         pass
 
     def fetch_latest_timestamp_of_symbol(
