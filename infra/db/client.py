977b2d3d8b94fe9db0118f49ce3b03c46a0aca40
tau <36781951+tndd@users.noreply.github.com>
2024-01-02 23:31:23 +0900

    add comment

--- infra/db/client.py
+++ infra/db/client.py
@@ -12,6 +12,9 @@ class PsqlClient:
 
 
     def transact(self, f, *args, **kwargs):
+        """
+        トランザクション処理のラッパー関数。
+        """
         conn = connect(self.url)
         cur = conn.cursor()
         try:
@@ -27,6 +30,9 @@ class PsqlClient:
 
 
     def calc_optimum_process_num(self, tasks: list) -> int:
+        """
+        実行に最適なプロセス数を計算する。
+        """
         return min(len(tasks), self.n_max_worker)
 
 
@@ -35,8 +41,26 @@ class PsqlClient:
         単発のクエリを実行し、結果を取得する。
         結果を返さないタイプのクエリの場合はNoneを返す。
         """
+        return self.execute_with_params(query, ())
+
+
+    def execute_queries(self, queries: List[str]):
+        """
+        複数のクエリを一気に実行する。
+        """
+        def _f(_cur, queries):
+            for query in queries:
+                _cur.execute(query)
+        self.transact(_f, queries)
+
+
+    def execute_with_params(self, query: str, params: Any) -> Any:
+        """
+        パラメータありで単発のクエリを実行し、結果を取得する。
+        結果を返さないタイプのクエリの場合はNoneを返す。
+        """
         def _f(_cur, query):
-            _cur.execute(query)
+            _cur.execute(query, params)
             if _cur.description is not None:
                 # 結果を返すクエリの場合
                 return _cur.fetchall()
@@ -45,17 +69,14 @@ class PsqlClient:
         return self.transact(_f, query)
 
 
-    def execute_queries(self, queries: List[str]):
-        def _f(_cur, queries):
-            for query in queries:
-                _cur.execute(query)
-        self.transact(_f, queries)
-
-
-    def execute_queries_with_params(self, queries_with_params: List[Tuple[str, tuple]]):
+    def execute_queries_with_params(self, queries_with_params: List[Tuple[str, Any]]):
         """
+        クエリとパラメータの複数ペアを一気に実行する。
+        こちらについてはexecute_with_paramsとは違い、結果を返さない。
+
         Note:
-            パラメータを渡す必要がないクエリの場合は空のタプル()を渡すようにすること。
+            1. パラメータを渡す必要がないクエリの場合は空のタプル()を渡すようにすること。
+            2. パラメータはdictあるいはtupleとして渡す。
         """
         def _f(_cur, queries_with_params):
             for query, params in queries_with_params:
@@ -64,12 +85,18 @@ class PsqlClient:
 
 
     def executemany(self, query: str, data: list):
+        """
+        単発のexecutemanyを実行。
+        """
         def _f(_cur, query, data):
             _cur.executemany(query, data)
         self.transact(_f, query, data)
 
 
     def parallel_execute(self, queries: List[str]):
+        """
+        複数のクエリを並列実行。
+        """
         n_process = self.calc_optimum_process_num(queries)
         with ProcessPoolExecutor(max_workers=n_process) as executor:
             for i in range(n_process):
@@ -78,6 +105,9 @@ class PsqlClient:
 
 
     def parallel_executemany(self, query: str, data: list):
+        """
+        大量のデータによるexecutemanyを並列実行で高速に実行する。
+        """
         n_process = self.calc_optimum_process_num(data)
         with ProcessPoolExecutor(max_workers=n_process) as executor:
             for i in range(n_process):
