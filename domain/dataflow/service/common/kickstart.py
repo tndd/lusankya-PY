a871db252217db59e87ad2e18e29d900e843680a
tau <36781951+tndd@users.noreply.github.com>
2024-01-04 06:42:50 +0900

    upd comment

--- domain/dataflow/service/common/kickstart.py
+++ domain/dataflow/service/common/kickstart.py
@@ -3,7 +3,7 @@ from domain.dataflow.repository.apiflow import ApiFlowRepository
 
 def kickstart_apiflow(rp_apiflow: ApiFlowRepository):
     """
-    未実行あるいは失敗したAPIを実行する
+    全エンドポイントの未実行あるいは失敗したAPIを無差別に実行する
     """
     api_requests = rp_apiflow.fetch_requests_not_executed_or_failed()
     pass
