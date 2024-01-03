bdbc4424da4cdebf2a068bbaebe688576f97c6a1
tau <36781951+tndd@users.noreply.github.com>
2024-01-03 21:30:16 +0900

    add request_api

---
+++ domain/dataflow/service/common/request_api.py
@@ -0,0 +1,11 @@
+from domain.dataflow.adapter import api_response_from_snapshot
+from domain.dataflow.model import ApiRequest, ApiResponse
+from infra.api.interface import rq_get
+
+
+def request_api(req: ApiRequest) -> ApiResponse:
+    """
+    dataflowドメイン形式のリクエストを引数に、同じくdataflowドメイン形式のレスポンスを返す
+    """
+    api_snapshot = rq_get(endpoint=req.endpoint, params=req.params, header=req.header)
+    return api_response_from_snapshot(snapshot=api_snapshot, api_request_id=req._id)
