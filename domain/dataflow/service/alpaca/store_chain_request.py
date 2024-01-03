1caca77c636977efcebccc6af3e6b1a0acd0cc6e
tau <36781951+tndd@users.noreply.github.com>
2024-01-03 19:42:25 +0900

    update store_chain_request

--- domain/dataflow/service/alpaca/store_chain_request.py
+++ domain/dataflow/service/alpaca/store_chain_request.py
@@ -1,7 +1,7 @@
-from domain.dataflow.api.adapter.api_result import \
-    snapshot_to_request_and_response
-from domain.dataflow.api.model import ApiRequest, ApiResponse
-from domain.dataflow.api.repository import ApiFlowRepository
+import json
+
+from domain.dataflow.adapter.api_result import api_result_from_snapshot
+from domain.dataflow.repository.apiflow import ApiFlowRepository
 from infra.api.alpaca.bar import AlpacaBarClient, QueryBar
 from infra.api.interface import ApiSnapshot
 
@@ -18,11 +18,11 @@ def store_chain_requests_bar(
         # データ取得
         snapshot: ApiSnapshot = api_cli_bar.get_bar(query)
         # snapshotの結果を変換し保存
-        rq, rp = snapshot_to_request_and_response(snapshot)
-        rp_api_flow.store_api_request(rq)
-        rp_api_flow.store_api_response(rp)
+        api_result = api_result_from_snapshot(snapshot)
+        rp_api_flow.store_api_result(api_result)
         # 次のページがない場合は終了
         if not 'next_page_token' in snapshot.r_body:
             break
         # query_barのnext_page_tokenを更新して再実行
-        query.page_token = snapshot.r_body['next_page_token']
+        body = json.loads(snapshot.r_body) # [Note]: r_bodyは文字列型なのでjson.loadsで辞書型に変換する
+        query.page_token = body['next_page_token']
